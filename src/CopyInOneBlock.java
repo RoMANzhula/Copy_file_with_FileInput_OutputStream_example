4b30ca8040fc685f9edaf4b08d8c8b5f8e72d262
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:47:13 +0200

    Update CopyInOneBlock.java
    Remove comments

--- src/CopyInOneBlock.java
+++ src/CopyInOneBlock.java
@@ -3,25 +3,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CopyInOneBlock {
-    public void copyAllBytesInOneBlock() throws IOException { //метод, который будет читать и записывать байты из файла в
-        //файл блоками размерностью равной столько, сколько мы указали в массиве байт (1024)
+    public void copyAllBytesInOneBlock() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("FromFile"); 
+        
+        FileOutputStream fileOutputStream = new FileOutputStream("ToFile"); 
+        
+        byte[] containerForBytes = new byte[1024];
 
-        FileInputStream fileInputStream = new FileInputStream("FromFile"); //открываем(создаем) поток для последовательного
-        // считывания байтов из указанного файла
-
-        FileOutputStream fileOutputStream = new FileOutputStream("ToFile"); //открываем поток для записи байтов в
-        //указанный файл
-
-        byte[] containerForBytes = new byte[1024]; //создаем контейнер в виде массива байт для записи данных блоками(частями)
-
-        while (fileInputStream.available() > 0) { //создаем цикл, который будет работать пока в файле будут доступные для чтения байты
-            int counterBytes = fileInputStream.read(containerForBytes); //создаем переменную для фиксации количества считанных байтов,
-            //при этом байты считываются в очередной блок
-            fileOutputStream.write(containerForBytes, 0, counterBytes); //в исходящий поток пишем байты из контейнера байтов,
-            //counterBytes выступает в роли контролера, он фиксирует последнюю партию, которая может быть от 1 до 1024
+        while (fileInputStream.available() > 0) {
+            int counterBytes = fileInputStream.read(containerForBytes);
+            fileOutputStream.write(containerForBytes, 0, counterBytes); 
         }
 
-        fileInputStream.close(); //очищаем ресурсы, закрываем поток для ввода данных
-        fileOutputStream.close(); //очищаем ресурсы, закрываем поток для вывода данных
+        fileInputStream.close();
+        fileOutputStream.close();
     }
 }
